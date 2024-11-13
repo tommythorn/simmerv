244b1a2efda9bc489c4c8a78d79d2db0c7aa62b4
Tommy Thorn <tommy-github2@thorn.ws>
2024-11-13 09:37:39 -0800

    Regenerated the wasm files

--- wasm/npm/nodejs/riscv_emu_rust_wasm_bg.wasm.d.ts
+++ wasm/npm/nodejs/riscv_emu_rust_wasm_bg.wasm.d.ts
@@ -1,7 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export function __wbg_wasmriscv_free(a: number): void;
+export function __wbg_wasmriscv_free(a: number, b: number): void;
 export function wasmriscv_new(): number;
 export function wasmriscv_setup_program(a: number, b: number, c: number): void;
 export function wasmriscv_load_program_for_symbols(a: number, b: number, c: number): void;
@@ -11,13 +11,14 @@ export function wasmriscv_run(a: number): void;
 export function wasmriscv_run_cycles(a: number, b: number): void;
 export function wasmriscv_run_until_breakpoints(a: number, b: number, c: number, d: number): number;
 export function wasmriscv_disassemble_next_instruction(a: number): void;
-export function wasmriscv_load_doubleword(a: number, b: number, c: number, d: number, e: number, f: number): void;
-export function wasmriscv_read_register(a: number, b: number, c: number): void;
-export function wasmriscv_read_pc(a: number, b: number): void;
+export function wasmriscv_load_doubleword(a: number, b: number, c: number, d: number, e: number): number;
+export function wasmriscv_read_register(a: number, b: number): number;
+export function wasmriscv_read_pc(a: number): number;
 export function wasmriscv_get_output(a: number): number;
 export function wasmriscv_put_input(a: number, b: number): void;
 export function wasmriscv_enable_page_cache(a: number, b: number): void;
-export function wasmriscv_get_address_of_symbol(a: number, b: number, c: number, d: number, e: number, f: number): void;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_free(a: number, b: number): void;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function wasmriscv_get_address_of_symbol(a: number, b: number, c: number, d: number, e: number, f: number): number;
+export const __wbindgen_export_0: WebAssembly.Table;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_start(): void;
--- wasm/npm/pkg/riscv_emu_rust_wasm.js
+++ wasm/npm/pkg/riscv_emu_rust_wasm.js
@@ -1,2 +1,5 @@
 import * as wasm from "./riscv_emu_rust_wasm_bg.wasm";
 export * from "./riscv_emu_rust_wasm_bg.js";
+import { __wbg_set_wasm } from "./riscv_emu_rust_wasm_bg.js";
+__wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
