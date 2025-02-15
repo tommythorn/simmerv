5f44888b84989317d2a2c89242e1e9e2c8f817a5
Tommy Thorn <tommy-github2@thorn.ws>
2025-02-15 10:25:48 -0800

    Refresh the WASM binaries

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,3 +1,4 @@
+cargo install wasm-bindgen-cli
 wasm-pack build
 wasm-bindgen ../target/wasm32-unknown-unknown/release/riscv_emu_rust_wasm.wasm --out-dir ./web --target web --no-typescript
 wasm-bindgen ../target/wasm32-unknown-unknown/release/riscv_emu_rust_wasm.wasm --out-dir ./npm/nodejs --nodejs
