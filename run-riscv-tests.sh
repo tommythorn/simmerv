e9d945d0a94bbc0bd178f7b2daae9520c9ad6d3b
Tommy Thorn <tommy.thorn>
2025-03-08 22:20:21 -0800

    riscv-tests/report.sh -> run-riscv-tests.sh for convenience

--- run-riscv-tests.sh
+++ run-riscv-tests.sh
@@ -2,8 +2,8 @@
 for class in passes fails sim-crash
 do echo
    echo "$class:"
-   for x in $class/*
+   for x in riscv-tests/$class/*
    do printf "%-25s " `basename $x`
-      ../target/release/riscv_emu_rust_cli 2>&1 $x|tail -2|head -1
+      target/release/riscv_emu_rust_cli 2>&1 $x|tail -2|head -1
    done
 done
