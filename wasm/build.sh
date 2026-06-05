2bbd4416b6d4503185d7edb977757826733c5575
Tommy Thorn <tommy-github2@thorn.ws>
2026-06-05 14:06:39 -0700

    Fix wasm-bindgen CLI version in build script

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,6 +1,6 @@
-# cargo install --locked wasm-pack wasm-bindgen-cli
-# 20260326 this seems to cause problems now and I had to use
+set -e
+
 cargo install --locked wasm-pack
-cargo install -f wasm-bindgen-cli --version 0.2.114
+cargo install -f wasm-bindgen-cli --version 0.2.108
 wasm-pack build
 wasm-bindgen ../target/wasm32-unknown-unknown/release/simmerv_wasm.wasm --out-dir ./web --target web --no-typescript
