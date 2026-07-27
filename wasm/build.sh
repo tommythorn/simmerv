87b279454bad94f8ddfaeeb66ba4c48a736d1fb2
Tommy Thorn <tommy-github2@thorn.ws>
2026-07-26 19:33:33 -0700

    CI wasm build appears to need an update to bindgen

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,6 +1,6 @@
 set -e
 
 cargo install --locked wasm-pack
-cargo install -f wasm-bindgen-cli --version 0.2.108
+cargo install -f wasm-bindgen-cli --version 0.2.126
 wasm-pack build
 wasm-bindgen ../target/wasm32-unknown-unknown/release/simmerv_wasm.wasm --out-dir ./web --target web --no-typescript
